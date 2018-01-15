121d4cc62ac06d7fdb9b319064713c667e45b5f2
sushree6678 <sushreesupravas96@gmail.com>
2018-01-15 11:32:20 +0530

    hhii

--- MyJavaProject/src/code/others/MySamleCode.java
+++ MyJavaProject/src/code/others/MySamleCode.java
@@ -5,7 +5,7 @@ public class MySamleCode {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World");
-		System.out.println("Hello India");
+		System.out.println("Hello Universe");
 	}
 
 }
